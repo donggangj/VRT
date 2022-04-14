09edeb0fd2630e22bcb454b18fa21431b8dc628c
Jiang, Donggang <donggang.jiang@intel.com>
2022-04-14 13:39:59 +0800

    1.Add gen_onnx.sh

---
+++ gen_onnx.sh
@@ -0,0 +1,1 @@
+python main_test_vrt.py --task 001_VRT_videosr_bi_REDS_6frames --folder_lq testsets/REDS4/sharp_bicubic --folder_gt testsets/REDS4/GT --tile 2 128 128 --tile_overlap 1 20 20 --to_onnx --verbose
